094524c3c6bf5a5eee6097e109efe2dfc2ac6a33
pjnoguerol <pjnoguerol@gmail.com>
2017-12-13 12:29:09 +0100

    Cambio a contactos multihilo y que no compruebe tu propio telefono

--- app/src/main/java/com/cineshared/pjnogegonzalez/cineshared/contactos/ContactosFragment.java
+++ app/src/main/java/com/cineshared/pjnogegonzalez/cineshared/contactos/ContactosFragment.java
@@ -42,6 +42,7 @@ public class ContactosFragment extends Fragment {
     private List<String> listaTelefonos;
     private RecyclerView recyclerView;
     private ProgressDialog carga;
+    private Usuarios usuario;
     private ConversionJson<Usuarios> conversionJson = new ConversionJson<>(Constantes.CONTACTOS);
 
     /**
@@ -57,8 +58,20 @@ public class ContactosFragment extends Fragment {
         Context context = inflater.getContext();
         View rootView = inflater.inflate(R.layout.recyclerview_activity, container, false);
         recyclerView = conversionJson.onCreateViewHistorico(context, rootView, getResources());
-        obtenerListaTelefonosContactos(context);
-        CargarContactos();
+        usuario = (Usuarios) getArguments().getSerializable(Constantes.USUARIOS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                context.checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1540);
+                new ContactosJsonTask().execute();
+
+
+            }
+            else
+            {
+                new ContactosJsonTask().execute();
+            }
+
+
         return rootView;
     }
     private void CargarContactos()
@@ -103,9 +116,9 @@ public class ContactosFragment extends Fragment {
     /**
      * Inner class que parsea la lista de contactos a una CardView
      */
-    public class ContactosJsonTask extends AsyncTask<URL, Void, List<Usuarios>> {
+    public class ContactosJsonTask extends AsyncTask<Void, Void, List<String>> {
 
-        private List<Usuarios> listaUsuarios;
+        private List<String> listaUsuarios;
 
 
         @Override
@@ -118,42 +131,13 @@ public class ContactosFragment extends Fragment {
         /**
          * Método que llama al parseo de contactos para obtener la lista a mostrar
          *
-         * @param urls URLs a conectar
+         * //@param urls URLs a conectar
          * @return Lista de contactos
          */
         @Override
-        protected List<Usuarios> doInBackground(URL... urls) {
-            return (listaUsuarios = conversionJson.doInBackground(urls));
-        }
-
-        /**
-         * Método que asigna la lista de contactos al adaptador para obtener un cardView
-         *
-         * @param listaUsuarios Lista de contactos para el adaptador
-         */
-        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
-        @Override
-        protected void onPostExecute(List<Usuarios> listaUsuarios) {
-            carga.dismiss();
-            recyclerView.setAdapter(conversionJson.onPostExecute(listaUsuarios));
-        }
-    }
-
-    /**
-     * Método que obtiene la lista de todos los teléfonos de la lista de contactos del usuario
-     *
-     * @param context Contexto para comprobar si se tienen los permisos necesarios
-     * @return Lista de teléfonos almacenados en el móvil
-     */
-    private List<String> obtenerListaTelefonosContactos(Context context) {
-        listaTelefonos = new ArrayList<>();
-        // Se comprueba que tenemos permiso para acceder a los contactos y en caso contrario se solicita
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                context.checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1540);
-        } else {
-            // Creamos las variables que nos permitan acceder al contenido del móvil y a los contactos
-            ContentResolver contentResolver = context.getContentResolver();
+        protected List<String> doInBackground(Void... vacio) {
+            List<String> listaTelefonos = new ArrayList<>();
+            ContentResolver contentResolver = getContext().getContentResolver();
             Cursor cursorContactos = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
             // Antes de continuar se comprueba que existan contactos en el teléfono, y los recorremos todos
             if (cursorContactos.getCount() > 0) {
@@ -167,39 +151,34 @@ public class ContactosFragment extends Fragment {
                         while (cursorContacto.moveToNext()) {
                             String phoneNo = cursorContacto.getString(cursorContacto.getColumnIndex(
                                     ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            listaTelefonos.add("'" + phoneNo.replace("+34", "").replaceAll("[ ()-]", "") + "'");
+                            String telefono = "'" + phoneNo.replace("+34", "").replaceAll("[ ()-]", "") + "'";
+                            if (usuario!=null )
+                                if (!usuario.getTelefono().equals(telefono))
+                                    listaTelefonos.add(telefono);
                         }
                         cursorContacto.close();
                     }
                 }
             }
+            return listaTelefonos;
         }
-        return listaTelefonos;
+
+        /**
+         * Método que asigna la lista de contactos al adaptador para obtener un cardView
+         *
+         * @param listaUsuarios Lista de contactos para el adaptador
+         */
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        @Override
+        protected void onPostExecute(List<String> listaUsuarios) {
+            carga.dismiss();
+            listaTelefonos = listaUsuarios;
+            CargarContactos();
+
+        }
     }
 
-    /**
-     * Método que comprueba de una lista de teléfonos cuáles tienen la aplicación instalada
-     *
-     * @param context Contexto para poder mostrar mensajes de error en caso de producirse
-     */
-    private void comprobarTelefonosAplicacion(Context context) {
-        if (listaTelefonos != null && !listaTelefonos.isEmpty()) {
-            try {
-                String listaTelefonosString = android.text.TextUtils.join(",", listaTelefonos);
-               // String url = Constantes.RUTA_CONTACTOS + listaTelefonosString;
-                //Log.w("CONTACTOS",url);
-                ConnectivityManager connMgr = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-                if (networkInfo != null && networkInfo.isConnected()) {
-                    //new ContactosJsonTask().execute(new URL(url));
-                } else {
-                    Toast.makeText(context, Constantes.ERROR_CONEXION, Toast.LENGTH_SHORT).show();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            Toast.makeText(context, "Error en contactos", Toast.LENGTH_SHORT).show();
-        }
-    }
+
+
+
 }
